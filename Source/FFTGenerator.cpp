a0d7af9e6b960e982db7078fbfa3d22f72b21d8a
Martin <martinpenberthy1@gmail.com>
2024-02-05 10:49:03 -0800

    Fixing font for freq markers

--- Source/FFTGenerator.cpp
+++ Source/FFTGenerator.cpp
@@ -159,17 +159,24 @@ void FFTGenerator::drawFrame (juce::Graphics& g)
                       (float) juce::jmap (i,     0, scopeSize - 1, 0, width), //endX
                               juce::jmap (scopeDataSummed[i],     0.0f, 1.0f, (float) heightScope, 0.0f) }); //endY
         
-        
-        //height += 20;
-        //100 hz
-        g.drawVerticalLine(width / 28, height - 10, height);
+        auto currFont = juce::Font(juce::String("Arial"), 20, 0);
+        g.setFont(currFont);
 
+        //100 hz
+        int x100hz = width / 28;
+        g.drawVerticalLine(x100hz, height - 5, height);
+        g.drawText(juce::String("100hz"), x100hz + 3, height - 15, 50, 20, juce::Justification::left);
         
         //1000 hz
-        g.drawVerticalLine(width / 5, height - 10, height);
+        int x1000hz = width / 5;
+        g.drawVerticalLine(x1000hz, height - 5, height);
+        g.drawText(juce::String("1khz"), x1000hz + 3, height - 15, 50, 20, juce::Justification::left);
+
         
         //5000 hz
-        g.drawVerticalLine(width / 1.5, height - 10, height);
+        int x5000hz = width / 1.5;
+        g.drawVerticalLine(x5000hz, height - 5, height);
+        g.drawText(juce::String("5khz"), x5000hz + 3, height - 15, 50, 20, juce::Justification::left);
 
             //g.drawText(juce::String((int)freq), x, height - 20, 45, 20, juce::Justification::centred);
         //}
